2b92a100f3e4d4d074514360ff3c4bf39574347f
ErionG14 <98408014+ErionG14@users.noreply.github.com>
2024-06-11 14:42:59 +0200

    first commit

---
+++ src/main/java/com/mycompany/calculatorapp/Calculatorapp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculatorapp;
+
+/**
+ *
+ * @author Erion Gosalci
+ */
+public class Calculatorapp {
+
+    public static void main(String[] args) {
+ 
+    }
+}
